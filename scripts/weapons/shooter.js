030e70517bf2826270a93577cce78d972146cb83
Kitunegit <kitsunesan220+git@gimal.com>
2023-10-09 20:44:18 +0900

    itemStartUseを変更

--- scripts/weapons/shooter.js
+++ scripts/weapons/shooter.js
@@ -1,30 +1,22 @@
 import { world, system, BlockPermutation } from "@minecraft/server";
 import * as Score from "../getscore.js";
-/*const itemTagMap = {
-  "spl:splatter_shot": "splatter_shooting",
-  "spl:splatter_dualies": "splatter_dualies_shot",
-  "spl:splatter_charger": "splatter_charger_shot",
-  "spl:splatter_spiner": "splatter_spiner_shot",
-  "spl:splatter_roller": "splatter_roller_shot",
-  "spl:brush": "brush_shot"
-};*/
-world.afterEvents.itemStartUse.subscribe(ev => {
-  /*  const { itemStack, source } = ev;
-  const itemId = itemStack.typeId;
 
-  const tag = itemTagMap[itemId];
-
-  if (tag) {
-      source.addTag(tag);
+const shooterweaponsData = {
+  "spl:splatter_shot": {
+      shotDelay: 2,
+      useInk:92,
+      eventName:"spl:splatter_shot"
   }
+};
 
-  if (itemId == `spl:splatter_charger`) {
-      for (const player of world.getPlayers({ excludeTags: ["sneaking"] })) {
-          if (Score.Get(`ink`, player) < 1800) {
-              player.sendMessage(`§l§cインク不足!`);
-          }
-      }
-  }*/
+world.afterEvents.itemStartUse.subscribe(ev => {
+  const { itemStack, source } = ev;
+  const weaponData = shooterweaponsData[itemStack.typeId];
+  
+  if(Score.Get(`ink`,source) > weaponData.useInk && Score.Get(`shot_cool`,source) < 0)
+  {
+    
+  }
 });
 
 world.afterEvents.itemStopUse.subscribe(ev => {
@@ -41,10 +33,4 @@ world.afterEvents.itemStopUse.subscribe(ev => {
       Score.Set(`spl_chrgr_count`, source, 0);
   }
 });
-const shooterweaponsData = {
-  splatter_shot: {
-      weaponId: "splatter_shot",
-      shotDelay: 2,
-      useInk:92
-  }
-};
+
